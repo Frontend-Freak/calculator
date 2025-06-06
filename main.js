c21a39a0bc15f6af7bd6e0ce40009129a41cadbb
Wh04m7 <slipknot.rulit3@gmail.com>
2025-06-07 01:00:12 +0300

    new

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
-const displayRes = document.getElementById('display').textContent;
-const buttons = Array.of(document.querySelectorAll('button'))
+const equalsButton = document.getElementById("equalsButton");
+const resultDiv = document.getElementById("resultDiv");
 
 
+function calculateResult() {
+    const a = Number(document.getElementById("firstOperand").value);
+    const b = Number(document.getElementById("secondOperand").value);
+    const operator = document.getElementById("action").value;
+    const result = document.getElementById("result").textContent;
 
+    if (isNaN(a) || isNaN(b)) {
+        alert("Ошибка");
+        return;
+    } else if (operator === "+") {
+        document.getElementById("result").textContent = a + b;
+    } else if (operator === "-") {
+        document.getElementById("result").textContent = a - b;
+    } else if (operator === "*") {
+        document.getElementById("result").textContent = a * b;
+    } else if (operator === "/") {
+        document.getElementById("result").textContent = a / b;
+    }
 
+    const newSpan = document.createElement("span");
+    newSpan.textContent = result;
+    resultDiv.appendChild(newSpan);
 
+    newSpan.addEventListener("click",() => {
+        resultDiv.removeChild(newSpan);
+    });
+    
+}
 
-console.log(buttons)
+equalsButton.addEventListener("click", calculateResult);
